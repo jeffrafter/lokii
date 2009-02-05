c3e0f40f7dd53bfc40e31c8821567d51fa9a2b5c
Jeff Rafter <jeff@baobabhealth.org>
2009-02-05 13:09:43 -0800

    More reorganization

--- templates/app/models/outbox.rb
+++
@@ -1,5 +0,0 @@
-class Outbox < ActiveRecord::Base
-  establish_connection(Lokii::Config.messages[Lokii::Config.environment]) if Lokii::Config.messages[Lokii::Config.environment]
-  named_scope :pending, :conditions => ['processed = 0']
-  set_table_name 'outbox'
-end  
--- templates/app/models/worker.rb
+++
@@ -1,3 +0,0 @@
-class Worker < ActiveRecord::Base
-  named_scope :active, :conditions => ['voided = 0 AND disabled = 0']
-end
